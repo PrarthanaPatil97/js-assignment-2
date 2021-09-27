6e2c079c1f759e587910e59adc5a5d692ab959c1
PrarthanaPatil97 <90627101+PrarthanaPatil97@users.noreply.github.com>
2021-09-27 10:42:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// WEB303 Assignment 2
+// Prarthana Purav Patil
+
+$('#vprospect').click(function()
+{
+	$('#solution').load('prospect.html');
+	
+});
+$('#vconvert').click(function()
+{
+	$('#solution').load('convert.html');
+	
+});
+$('#vretain').click(function()
+{
+	$('#solution').load('retain.html');
+	
+});
+
